cce77ba16075a9e6bb275522d9428b86df05bcc0
unwired <unwired@tutanota.de>
2025-02-19 18:01:05 +0100

    Dodano zadanie 13

--- zadania_operon/zad13.py
+++ zadania_operon/zad13.py
@@ -35,7 +35,7 @@ def zad_13_1():
             licznik += 1
     print(licznik)
 
-def zad_13_2():
+def zad_13_2(baza):
     konwersje = {}
     licznik = 0
     plik = open(r"C:\Kuba\informatyka\informatyka-repetytorium\rozdział 2\pliki\13\liczby13.txt", "r")
@@ -43,25 +43,57 @@ def zad_13_2():
     max = 0
     for x in liczby:
         licznik = 0
-        konwersje[x] = zamien(x,5)
+        konwersje[x] = zamien(x, baza)
         for y in konwersje[x]:
-            if y == 4:
+            if y == baza - 1:
                 licznik += 1
         if licznik > max:
             max = licznik
     for x in konwersje:
         licznik = 0
         for y in konwersje[x]:
-            if y == 4:
+            if y == baza - 1:
                 licznik += 1
         if licznik == max:
             print(x)
 
     pass
 
-def zad_13_3():
+def zad_13_3(baza):
+    konwersje = {}
+    licznik = 0
+    plik = open(r"C:\Kuba\informatyka\informatyka-repetytorium\rozdział 2\pliki\13\liczby13.txt", "r")
+    liczby = plik.readlines()
+    max = 0
+    for x in liczby:
+        licznik = 0
+        konwersje[x] = zamien(x, baza)
+        tablica = konwersje[x]
+        dlugosc = len(tablica)
+        tab1 = tablica[:dlugosc // 2]
+        tab2 = tablica[-(dlugosc - 1) // 2:]
+        dlugosc2 = len(tab2) - 1
+        tab3 = []
+        for y in range(len(tab2)):
+            tab3.append(y)
+        for z in range(len(tab2)):
+            tab3[dlugosc2] = tab2[z]
+            dlugosc2 -= 1
+        rowne = True
+        for z in range(len(tab3)):
+            if tab1[z] != tab3[z]:
+                rowne = False
+                break
+        if rowne:
+            print(x)
+
+
+
+
     pass
 
 #zad_13_1()
-zad_13_2( )
-#zad_13_3()
+#zad_13_2(5)
+#zad_13_2(7)
+#zad_13_2(13)
+zad_13_3(16)
