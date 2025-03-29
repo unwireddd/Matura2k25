9c09ed8e1f24b9d819dadbb1e7f41bf88d341214
unwired <unwired@tutanota.de>
2025-03-29 23:29:42 +0100

    Dodano zadanie 24.

--- arkusze/2023/2022/zad3.py
+++ arkusze/2023/2022/zad3.py
@@ -1,6 +1,8 @@
+import math
+
 def czy_pierwsza(liczba):
-    ile = 0
-    for x in range(1, liczba):
+    c = math.ceil(math.sqrt(liczba))
+    for x in range(2, c + 1):
         #print(x)
 
         if liczba % x == 0:
@@ -24,14 +26,23 @@ def ile_pierwszych():
 
 def goldbach():
     rozklady = []
-    plik = open("/home/elliot/Downloads/Dane_2212/liczby.txt", "r")
+    plik = open(r"C:\Kuba\Dane_2212\liczby.txt", "r")
     liczby_str = plik.readlines()
     liczby = []
     ile = 0
     for x in liczby_str:
         liczby.append(int(x))
-    
+
+    pierwsze = {}
+    for x in range(4, 1000001):
+        print(f"spr {x}")
+        if czy_pierwsza(x):
+            pierwsze[x] = True
+
+    lic = 1
     for x in liczby:
+        print(f"sprawdzam {lic} : {x}")
+        lic += 1
         ileroz = 0
         for i in range(1, x):
             if czy_pierwsza(i) and czy_pierwsza(x-i):
