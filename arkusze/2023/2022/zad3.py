bf26501d6bf72e9cc6d942cce707b28f96eddd7a
unwired <unwired@tutanota.de>
2025-03-30 04:05:57 +0200

    up

--- arkusze/2023/2022/zad3.py
+++ arkusze/2023/2022/zad3.py
@@ -26,7 +26,7 @@ def ile_pierwszych():
 
 def goldbach():
     rozklady = []
-    plik = open(r"C:\Kuba\Dane_2212\liczby.txt", "r")
+    plik = open("/home/elliot/Downloads/Dane_2212/liczby.txt", "r")
     liczby_str = plik.readlines()
     liczby = []
     ile = 0
@@ -48,8 +48,57 @@ def goldbach():
             if czy_pierwsza(i) and czy_pierwsza(x-i):
                 ileroz += 1
         rozklady.append(ileroz)
-    ileroz = sorted(ileroz)
-    print(ileroz(len(ileroz)-1))
+        rozklady = sorted(rozklady)
+        print(rozklady[len(rozklady)-1])
+    #jak cos to to sie wykonuje nie wiadomo czemu jak tego nie usune
+    #dobra to ogolnie bedzie dzialac ale wykonanie tego to zajmie godzine wiec polecam sprawdzic gdzie indziej rozwiazanie
+
+def szesnastkowe():
+    plik = open("/home/elliot/Downloads/Dane_2212/liczby.txt", "r")
+    liczby_str = plik.readlines()
+    liczby = []
+    ile = 0
+    for x in liczby_str:
+        liczby.append(int(x))
+    cyfry = {
+        0: 0,
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0,
+        6: 0,
+        7: 0,
+        8: 0,
+        9: 0,
+        "A": 0,
+        "B": 0,
+        "C": 0,
+        "D": 0,
+        "E": 0,
+        "F": 0,
+    }
+    for x in liczby:
+        for x in range(x-1):
+            while x > 0:
+                reszta = x % 16
+                if reszta < 10:
+                    cyfry[reszta] += 1
+                elif reszta == 11:
+                    cyfry["A"] += 1
+                elif reszta == 12:
+                    cyfry["B"] += 1
+                elif reszta == 13:
+                    cyfry["C"] += 1
+                elif reszta == 14:
+                    cyfry["D"] += 1
+                elif reszta == 15:
+                    cyfry["E"] += 1
+                elif reszta == 16:
+                    cyfry["F"] += 1
+                x = x // 16
+    print(cyfry)
 
 
-goldbach()
+
+szesnastkowe()
